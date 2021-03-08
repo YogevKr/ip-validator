14e7b5b59c3adf952bd3e55136bffd66ead6ff83
Yogev Kriger <yogev@appcard.com>
2021-03-08 10:43:44 +0200

    init

---
+++ app.py
@@ -0,0 +1,44 @@
+import ipaddress
+import streamlit as st
+
+
+IP_PLACEHOLDER = 'XXX.XXX.XXX.XXX'
+GATEWAY_PLACEHOLDER = 'XXX.XXX.XXX.XXX'
+MASK_PLACEHOLDER = 'XXX.XXX.XXX.XXX'
+
+
+def validation(ip: str, gateway: str,mask: str):
+    try:
+        ip_ = ipaddress.IPv4Address(ip)
+        if  ip_ not in ipaddress.IPv4Network(f"{gateway}/{mask}", False).hosts():
+            st.write("Error: IP not in range")
+        else: 
+            st.write("Valid.")
+            st.write("IP address:", ip)
+            st.write("Gateway:", gateway)
+            st.write("Mask:", mask)
+
+            if not ip_.is_private:
+                st.write("Notice! this ip is not in a standart private range.")
+
+    except Exception as e:
+        st.write(e)
+    
+
+
+st.title('IP validator')
+
+
+ip = st.text_input('IP address', value=IP_PLACEHOLDER)
+gateway = st.text_input('Gateway', value=GATEWAY_PLACEHOLDER)
+mask = st.text_input('Mask', value=GATEWAY_PLACEHOLDER)
+
+if st.button('Validate'):
+    if ip == IP_PLACEHOLDER:
+        st.write("Please fill an IP address")
+    elif gateway == GATEWAY_PLACEHOLDER:
+        st.write("Please fill a Gateway address")
+    elif mask == MASK_PLACEHOLDER:
+        st.write("Please fill a mask")
+    else:
+        validation(ip,gateway,mask) 

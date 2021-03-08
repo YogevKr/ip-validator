a0b8e31e8d29ff4b41f14f4f024063d8a88a1625
Yogev Kriger <yogev@appcard.com>
2021-03-08 11:10:42 +0200

    black

--- app.py
+++ app.py
@@ -2,38 +2,36 @@ import ipaddress
 import streamlit as st
 
 
-IP_PLACEHOLDER = 'XXX.XXX.XXX.XXX'
-GATEWAY_PLACEHOLDER = 'XXX.XXX.XXX.XXX'
-MASK_PLACEHOLDER = 'XXX.XXX.XXX.XXX'
+IP_PLACEHOLDER = "XXX.XXX.XXX.XXX"
+GATEWAY_PLACEHOLDER = "XXX.XXX.XXX.XXX"
+MASK_PLACEHOLDER = "XXX.XXX.XXX.XXX"
 
 
-def validation(ip: str, gateway: str,mask: str):
+def validation(ip: str, gateway: str, mask: str):
     try:
         ip_ = ipaddress.IPv4Address(ip)
-        if  ip_ not in ipaddress.IPv4Network(f"{gateway}/{mask}", False).hosts():
+        if ip_ not in ipaddress.IPv4Network(f"{gateway}/{mask}", False).hosts():
             st.write("Error: IP not in range")
-        else: 
-            st.write("Valid.")
+        else:
+            if not ip_.is_private:
+                st.header("Warning! This ip is not in a standard private range.")
+
+            st.subheader("Valid.")
             st.write("IP address:", ip)
             st.write("Gateway:", gateway)
             st.write("Mask:", mask)
 
-            if not ip_.is_private:
-                st.write("Notice! this ip is not in a standart private range.")
-
     except Exception as e:
         st.write(e)
-    
 
 
-st.title('IP validator')
+st.title("IP validator")
 
+ip = st.text_input("IP address", value=IP_PLACEHOLDER)
+gateway = st.text_input("Gateway", value=GATEWAY_PLACEHOLDER)
+mask = st.text_input("Mask", value=GATEWAY_PLACEHOLDER)
 
-ip = st.text_input('IP address', value=IP_PLACEHOLDER)
-gateway = st.text_input('Gateway', value=GATEWAY_PLACEHOLDER)
-mask = st.text_input('Mask', value=GATEWAY_PLACEHOLDER)
-
-if st.button('Validate'):
+if st.button("Validate"):
     if ip == IP_PLACEHOLDER:
         st.write("Please fill an IP address")
     elif gateway == GATEWAY_PLACEHOLDER:
@@ -41,4 +39,4 @@ if st.button('Validate'):
     elif mask == MASK_PLACEHOLDER:
         st.write("Please fill a mask")
     else:
-        validation(ip,gateway,mask) 
+        validation(ip, gateway, mask)
